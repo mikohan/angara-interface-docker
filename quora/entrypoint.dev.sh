ab9958f6dd90a8ece0626a642e0c31093657d0a2
mikohan <angara99@gmail.com>
2022-11-17 10:47:37 +0300

    Clean up

--- quora/entrypoint.dev.sh
+++ quora/entrypoint.dev.sh
@@ -3,6 +3,7 @@
 
 python manage.py migrate --no-input
 python manage.py collectstatic --no-input
+python manage.py cleanup_unused_media --no-input
 
 
 # gunicorn quora.wsgi:application  -w 8 --bind 0.0.0.0:8003
